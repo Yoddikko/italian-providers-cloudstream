8086d4eb9f1781dc23f94b112f5c901372d72621
Alessio <alessioiodiceuni@gmail.com>
2022-09-07 12:49:09 +0200

    CB01 provider added

--- ExampleProvider/src/main/kotlin/com/example/ExampleProvider.kt
+++ ExampleProvider/src/main/kotlin/com/example/ExampleProvider.kt
@@ -4,8 +4,8 @@ import com.lagradost.cloudstream3.TvType
 import com.lagradost.cloudstream3.MainAPI
 import com.lagradost.cloudstream3.SearchResponse
 
-class ExampleProvider : MainAPI() { // all providers must be an instance of MainAPI
-    override var mainUrl = "https://cb01.uno/" 
+class CB01 : MainAPI() { // all providers must be an instance of MainAPI
+    override var mainUrl = "https://cb01.kaufen" 
     override var name = "CB01"
     override val supportedTypes = setOf(TvType.Movie)
 
