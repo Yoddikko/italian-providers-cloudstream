5728aeb8be1b614062c4ee98a264bc85ce0b0fba
Alessio <alessioiodiceuni@gmail.com>
2022-09-12 10:04:28 +0200

    The home should work now

--- ExampleProvider/build.gradle.kts
+++ ExampleProvider/build.gradle.kts
@@ -1,11 +1,11 @@
 // use an integer for version numbers
-version = 1
+version = 2
 
 
 cloudstream {
     // All of these properties are optional, you can safely remove them
 
-    description = "Lorem Ipsum"
+    description = "CB01 provider"
     authors = listOf("Cloudburst")
 
     /**
